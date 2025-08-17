8d4281bd751eb1bd43b55421dac49571ef2f7eaa
Abhi1921-creator <abhigarg1921@gmail.com>
2025-08-18 03:04:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config(); // Load .env variables
 const app = express();
 const port = 3000;
 
-app.use(express.static("Public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
